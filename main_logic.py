c458ea57db04266a7d80dd530480d3c037861b6a
KhoalaS <s4khleee@uni-trier.de>
2022-06-07 18:01:01 +0200

    add console log widget

--- main_logic.py
+++ main_logic.py
@@ -1,7 +1,10 @@
 import os
 import zipfile as zipF
 
-def extract(filename, should_d2j, is_mod):
+from PyQt5.QtCore import QMetaObject, Qt, Q_ARG
+
+
+def extract(filename, should_d2j, is_mod, obj):
     os.system('pwd')
     prefix = 'org'
     if is_mod:
@@ -31,25 +34,39 @@ def extract(filename, should_d2j, is_mod):
         name, x = dex.split('.dex')
         classes_zip.append(name + '.zip')
         if should_d2j:
-            os.system('bash ../d2j-dex2jar.sh --force {}_classes/{} -o {}_classes/jars/{}'.format(prefix, dex, prefix,
-                                                                                               name + '.zip'))
+            QMetaObject.invokeMethod(obj,
+                                     "updateStatus", Qt.QueuedConnection,
+                                     Q_ARG(str, 'dex2jar file: {} from {} apk'.format(dex, prefix)), Q_ARG(bool, False))
+            os.system('bash ../d2j-dex2jar.sh --force {}_classes/{} -o {}_classes/jars/{}'.format(prefix, dex, prefix, name + '.zip'))
+
+    QMetaObject.invokeMethod(obj,
+                             "updateStatus", Qt.QueuedConnection,
+                             Q_ARG(str, 'DEX2JAR FINISHED FOR {} APK'.format(prefix)), Q_ARG(bool, True))
+
 
     if not os.path.exists(prefix + '_classes/all_classes'):
         os.mkdir(prefix + '_classes/all_classes')
 
     for zip_class in classes_zip:
         class_zip = prefix + '_classes/jars/' + zip_class
+        QMetaObject.invokeMethod(obj,
+                                 "updateStatus", Qt.QueuedConnection,
+                                 Q_ARG(str, 'unzipping file: ' + zip_class), Q_ARG(bool, False))
         os.system('unzip -o {} -d {}'.format(class_zip, prefix + '_classes/all_classes/'))
 
+    QMetaObject.invokeMethod(obj,
+                             "updateStatus", Qt.QueuedConnection,
+                             Q_ARG(str, 'UNZIP FINISHED FOR {} APK'.format(prefix)), Q_ARG(bool, True))
     org_filepaths = open(prefix + '_filepaths.txt', 'w+')
     for root, dirs, files in os.walk(os.path.relpath(prefix + "_classes/all_classes")):
         for file in files:
             org_filepaths.write(root + '/' + file + '\n')
 
     os.rename(org_zipped_name, org_filepath)
+    org_filepaths.close()
 
 
-def compare(gran, mainfolder):
+def compare(gran, mainfolder, obj):
     os.chdir(mainfolder)
     org_files = open('org_filepaths.txt', 'r')
     file_missing = open('missing.txt', 'w+')
@@ -60,13 +77,18 @@ def compare(gran, mainfolder):
             print('File doesnt exist in mod')
             file_missing.write(file)
             continue
+        QMetaObject.invokeMethod(obj,
+                                 "updateStatus", Qt.QueuedConnection,
+                                 Q_ARG(str, 'comparing file: '+file.strip()), Q_ARG(bool, False))
         mod_size = os.stat(file.strip().replace('org_classes', 'mod_classes')).st_size
-        print('comparinf file:{}'.format(file.strip()))
         if abs(org_size - mod_size) > gran:
             file_diff.write(file)
     org_files.close()
     file_missing.close()
     file_diff.close()
+    QMetaObject.invokeMethod(obj,
+                             "updateStatus", Qt.QueuedConnection,
+                             Q_ARG(str, 'COMPARING FINISHED'), Q_ARG(bool, True))
 
 
 def files_added():
@@ -78,12 +100,15 @@ def files_added():
     for file in mod_files:
         if file.replace('mod_classes', 'org_classes') not in org_f:
             add_files.write(file)
+    org_files.close()
+    mod_files.close()
+    add_files.close()
 
 # extract('spotify_org.apk', False, False)
 # extract('spotify_mod.apk', False, True)
 # compare(100)
 # files_added()
-def copy_diffs(mainfolder):
+def copy_diffs(mainfolder, obj):
     os.chdir(mainfolder)
     if not os.path.exists('diff'):
         os.mkdir('diff')
@@ -93,12 +118,18 @@ def copy_diffs(mainfolder):
         os.mkdir('diff/mod')
     diff_org = open('diff.txt', 'r')
     for file in diff_org:
+        QMetaObject.invokeMethod(obj,
+                                 "updateStatus", Qt.QueuedConnection,
+                                 Q_ARG(str, 'copying file: '+file.strip()), Q_ARG(bool, False))
         os.system('cp {} diff/org/'.format("'"+file.strip()+"'"))
         os.system('cp {} diff/mod/'.format("'"+file.strip().replace('org_classes', 'mod_classes')+"'"))
     diff_org.close()
-    print('copy done')
+    QMetaObject.invokeMethod(obj,
+                             "updateStatus", Qt.QueuedConnection,
+                             Q_ARG(str, 'COPY FINISHED'), Q_ARG(bool, True))
 
-def decompile(mainfolder):
+
+def decompile(mainfolder, obj):
     os.chdir(mainfolder)
     if not os.path.exists('diff/org/java'):
         os.makedirs('diff/org/java', exist_ok=True)
@@ -106,5 +137,14 @@ def decompile(mainfolder):
         os.makedirs('diff/mod/java',exist_ok=True)
     diff_org = open('diff.txt', 'r')
     for file in diff_org:
+        QMetaObject.invokeMethod(obj,
+                                 "updateStatus", Qt.QueuedConnection,
+                                 Q_ARG(str, 'decompiling file: '+file.strip()), Q_ARG(bool, False))
         os.system('java -jar ../jd-cli.jar -od {} {}'.format('diff/org/java', "'"+file.strip()+"'"))
         os.system('java -jar ../jd-cli.jar -od {} {}'.format('diff/mod/java',"'"+file.strip().replace('org_classes', 'mod_classes')+"'"))
+    QMetaObject.invokeMethod(obj,
+                             "updateStatus", Qt.QueuedConnection,
+                             Q_ARG(str, 'DECOMPILE FINISHED'), Q_ARG(bool, True))
+    QMetaObject.invokeMethod(obj,
+                             "showFiles", Qt.QueuedConnection,)
+
